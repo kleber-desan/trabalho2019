67af769f99386ea02a59c2e11159636e7895d26e
kleber-desan <53978705+kleber-desan@users.noreply.github.com>
2019-08-23 22:21:45 -0300

    aula23-08

---
+++ App/Xfw/Xfw/Models/Services/ItemService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xfw.Models;
+
+
+
+namespace Xfw.Models.Services
+{
+    public interface IItemService
+
+    {
+        Task<List<Item>> GetAll();
+
+
+    }
+    public sealed class ItemService : IItemService
+    {
+        public async Task<List<Item>> GetAll()
+        {
+            await Task.Delay(2000);
+            return new List<Item>
+            
+            {
+                Item.Create("Cebolinha","","https://img.elo7.com.br/product/zoom/2135A33/roupa-cebolinha-monica.jpg"),
+                Item.Create("Magali","",""),
+                Item.Create("cascao","","https://pt.wikipedia.org/wiki/Ficheiro:Cascao.png"),
+                Item.Create("Loco","",""),
+
+            };
+            
+        }
+    }
+}
---
+++ App/Xfw/Xfw/Views/HomePage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xfw.Models;
+using Xfw.ViewModel;
+
+namespace Xfw.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        private HomeViewModel ViewModel;
+
+
+        public HomePage()
+        {
+            InitializeComponent();
+            ViewModel = new HomeViewModel();
+            BindingContext = ViewModel;
+
+        }
+        protected override async void OnAppearing()
+        {
+            await ViewModel.Initialize();
+
+        }
+
+        private async void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e.Item == null)
+                return;
+
+            var gangMember = e.Item as Item;
+                                 
+
+            await Navigation.PushAsync(new DetailPage());
+
+        }
+    }
+}
